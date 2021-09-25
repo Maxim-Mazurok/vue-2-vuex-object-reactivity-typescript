d24dd64d5d7191394a33c8b05823ea05ffe386f3
Maxim Mazurok <maxim@mazurok.com>
2021-09-25 19:37:03 +1000

    typescript parser

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,11 +6,15 @@ module.exports = {
   extends: [
     'plugin:vue/essential',
     '@vue/standard',
-    '@vue/typescript/recommended'
+    '@vue/typescript/recommended',
+    'plugin:@typescript-eslint/recommended'
   ],
+  parser: 'vue-eslint-parser',
   parserOptions: {
+    parser: '@typescript-eslint/parser',
     ecmaVersion: 2020
   },
+  plugins: ['@typescript-eslint'],
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
